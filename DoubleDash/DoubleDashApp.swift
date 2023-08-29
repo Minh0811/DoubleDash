11d485b389b3787b45f5f177898dd9c78a905662
minh0811 <vokhaiminh0811@gmail.com>
2023-08-29 17:27:25 +0700

    Initial Commit

---
+++ DoubleDash/DoubleDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleDashApp.swift
+//  DoubleDash
+//
+//  Created by Minh Vo on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DoubleDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
